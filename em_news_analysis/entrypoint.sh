5cc2d5e32f0df6bf3b953d0fba0f4d0dc0777aac
Raffay Rana <ar534@duke.edu>
2024-09-09 21:27:36 -0400

    Add debugging

--- em_news_analysis/entrypoint.sh
+++ em_news_analysis/entrypoint.sh
@@ -4,5 +4,9 @@ set -e
 # Generate the config file
 /app/generate_config.sh
 
+# Debug: Print the contents of the generated file
+echo "Generated config file:"
+cat /app/config/em-news-gdelt.json
+
 # Run the application
 exec poetry run python news_pipeline_server.py
