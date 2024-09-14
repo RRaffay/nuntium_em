1df579bfe366f32760abe202fffe375665996db3
Raffay Rana <ar534@duke.edu>
2024-09-14 15:36:35 -0400

    Logging for backend

--- backend/app/auth/users.py
+++ backend/app/auth/users.py
@@ -14,6 +14,10 @@ from .email_utils import send_verification_email, send_password_reset_email
 from auth.auth_db import User, get_user_db
 from auth.schemas import UserCreate
 from config import settings
+import logging
+
+settings.setup_logging()
+logger = logging.getLogger(__name__)
 
 SECRET = settings.JWT_SECRET
 
@@ -23,24 +27,25 @@ class UserManager(ObjectIDIDMixin, BaseUserManager[User, PydanticObjectId]):
     verification_token_secret = SECRET
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
-        print(f"User {user.id} has registered.")
+        logger.info(f"User {user.id} has registered.")
 
     async def on_after_forgot_password(
         self, user: User, token: str, request: Optional[Request] = None
     ):
         await send_password_reset_email(user.email, token)
-        print(f"User {user.id} has forgot their password. Reset token: {token}")
+        logger.info(
+            f"User {user.id} has forgot their password. Reset token: {token}")
 
     async def on_after_request_verify(
         self, user: User, token: str, request: Optional[Request] = None
     ):
         await send_verification_email(user.email, token)
-        print(
+        logger.info(
             f"Verification requested for user {user.id}. Verification token: {token}")
 
     async def on_after_verify(self, user: User, request: Optional[Request] = None):
         user.is_verified = True
-        print(f"User {user.id} has verified their email.")
+        logger.info(f"User {user.id} has verified their email.")
 
 
 async def get_user_manager(user_db: BeanieUserDatabase = Depends(get_user_db)):
