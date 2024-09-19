708c3a310297785e03c72d70c293fc72e40763eb
Raffay Rana <ar534@duke.edu>
2024-09-19 12:47:39 -0400

    Header modifications

--- em_research_chat/em_research_chat/utils/article_summarizer.py
+++ em_research_chat/em_research_chat/utils/article_summarizer.py
@@ -92,13 +92,7 @@ def article_summarizer(url: str, model: int = 3, max_words: int = 50000) -> str:
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.5",
-            "Referer": "https://www.google.com/",
-            "DNT": "1",
-            "Connection": "keep-alive",
-            "Upgrade-Insecure-Requests": "1",
-            "Cache-Control": "max-age=0",
-            "Pragma": "no-cache",
-            "Accept-Encoding": "gzip, deflate, br"
+            "Referer": "https://www.google.com/"
         }
         loader = WebBaseLoader(url, header_template=custom_headers)
     docs = loader.load()
